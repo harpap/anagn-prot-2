660b9a512dff65d2d16773ff73342b888385c0fd
harpap <10978002+harpap@users.noreply.github.com>
2019-05-17 20:30:09 +0300

    we

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 import data_parser
 import numpy as np
 from sklearn import metrics
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, SpectralClustering, AgglomerativeClustering
 
 def main(args):
 
@@ -18,28 +18,23 @@ def main(args):
         return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix) 
 
     def f1_score(y_true, y_pred, max_clusters):
-        # compute contingency matrix (also called confusion matrix)
-        contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
-        # pleiopsifousa katigoria
-        max = np.amax(contingency_matrix, axis=0)
         y_true_c = []
         y_pred_c = []
         sum = 0
-        for i in range(2):
+        for i in range(max_clusters):
             for j in range(len(y_true)):
-                if (i==y_true[j]):
+                if (i==y_pred[j]):
                     y_pred_c.append(y_pred[j])
-            print('ypred')
-            print (y_pred_c)
-            counts = np.bincount(y_pred_c)
-            y_true_c = [np.argmax(counts)] * len(y_pred_c)
-            print('counts')
-            print(counts)
-            print('shapes')
-            print(len(y_true_c))
-            print(len(y_pred_c))
-            #y_true_c = max...
-            sum += metrics.f1_score(y_true_c, y_pred_c, average = None)# isws kanto esu
+                    y_true_c.append(y_true[j])
+            counts = np.bincount(y_true_c)
+            y_pred_c = [np.argmax(counts)] * len(y_pred_c)
+            sum += metrics.f1_score(y_true_c, y_pred_c)
+            print('y_pred_c')
+            print(y_pred_c)
+            print('y_true_c')
+            print(y_true_c)
+            print('inside f1')
+            print (metrics.f1_score(y_true_c, y_pred_c))
             y_true_c = []
             y_pred_c = []
         return sum
@@ -52,13 +47,21 @@ def main(args):
             # The final results will be the best output of 10 consecutive runs in terms of inertia.
             clusterer = KMeans(n_clusters=n_clusters, random_state=10)
             cluster_labels = clusterer.fit_predict(data)
-            #for x in cluster_labels:
-            # print(x)
             #print('purity')
             #print (purity_score(target, cluster_labels))
             print('f1')
             print (f1_score(target, cluster_labels, n_clusters))
-            #print(metrics.f1_score(target, cluster_labels, average = 'weighted'))
+    
+    def SPC():
+        #for n_clusters in range_n_clusters:
+            clusterer = SpectralClustering(n_clusters=2, assign_labels="discretize", random_state=0)
+            cluster_labels = clusterer.fit_predict(data[:100])
+            print(cluster_labels)
+    
+    def AMC():
+        for n_clusters in range_n_clusters:
+            clusterer = AgglomerativeClustering(n_clusters=2)
+            cluster_labels = clusterer.fit_predict(data[:100])
     
     range_n_clusters = [2, 4, 8]
     options = {'occupancy1' : data_parser.load_occupancy_data1,
@@ -70,11 +73,12 @@ def main(args):
     
     if len(sys.argv)==2:
         kMNS()
-        GNB()
-        MLP()
-        SVM()        
+        SPC()
+        AMC()     
     else:
-        options = {'kmeans' : kMNS
+        options = {'kmeans'         : kMNS,
+                   'spectral'       : SPC,
+                   'agglomerative'  : AMC
         }
         options[args.method]()
     
@@ -84,7 +88,7 @@ def parse_arguments(argv):
     parser.add_argument('dataset', type=str, 
         help='which dataset to be loaded, options: for datatest.txt type \'occupancy1\', for datatest2.txt type \'occupancy2\', for datatraining.txt type \'occupancy3\', or type \'spambase\'')
     parser.add_argument('--method', type=str, 
-        help='which method to be executed, options: \'kmeans\', \'gnb\', \'mlp\', \'svm\'', default='')
+        help='which method to be executed, options: \'kmeans\', \'spectral\', \'agglomerative\'', default='')
     return parser.parse_args(argv)
 
 if __name__ == '__main__':
